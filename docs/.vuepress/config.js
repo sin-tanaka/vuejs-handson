849293bb92647f43c24b31f09e12e329233edb91
Shintaro Tanaka <sin_tanaka@jsl.co.jp>
2019-01-09 16:42:11 +0900

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,11 @@ module.exports = {
   base: '/vuejs-handson/',
   title: 'JSL Vuejs Hands-on',
   description: 'JSLで開催されたVuejsのハンズオン資料ページです',
+  repo: 'sin-tanaka/vuejs-handson',
+  docsRepo: 'sin-tanaka/vuejs-handson',
+  docsDir: 'docs',
+  docsBranch: 'master',
+  editLinks: true,
   locales: {
     '/': {
       lang: 'ja'
